ddd3ee49a40211c17b52715efcb1df57695b170a
TalhaKhawaja <talhanaik@gmail.com>
2021-05-11 14:15:13 -0400

    portfolio-v1: added additional data to the config

--- src/config.js
+++ src/config.js
@@ -2,31 +2,51 @@ module.exports = {
     navLinks: [
         {
             name: "About",
-            url: "/about"
+            url: "/#about"
         }, {
             name: "Experience",
-            url: "/exp"
+            url: "/#experience"
         }, {
             name: "Projects",
-            url: "/projects"
+            url: "/#projects"
         }, {
             name: "Contact",
-            url: "/contact"
+            url: "/#contact"
         }
     ],
     sections: [
         {
-            name: "About",
-            data: "Hi my name is Talha Khawaja! I am a Software Engineer"
+            about: {
+                salutation: 'Hello!',
+                intro: 'I am Talha Khawaja',
+                info: 'I am a Software Engineer based in Long Island, NY',
+                additionalInfo: 'I have worked on frontend and backend development in my software engineering career'
+            },
+            experience: [{
+                startDate: 'Feburary 2017',
+                endDate: 'December 2020',
+                jobTitle: 'Software Engineer',
+                employer: 'PowerSchool Group',
+                city: 'Folsom, CA',
+                description: [{
+                    1: 'Worked on PowerSchool Design System internal library to create and refine Polymer and Angular components',
+                    2: 'Worked on Case Management using TypeScript and Polymer to create a functionality that allows users to track a student' / 's progress through the Special Education process.',
+                    3: 'Developed an WPF AES key generator application and back-end encryption process to encrypt and decrypt specific values in the database using C#.',
+                    4: 'Full stack software development using TypeScript, C#, MSSQL, HTML/CSS for our K-12 products.',
+                    5: 'Worked on features and enhancements to Special Programs K-12 web application using C#, ASP.NET and JavaScript.',
+                    6: 'Worked through all phases of the Agile software development life cycle including design, development, testing.'
+                }]
+            }]
+        }
+    ],
+    socialMedia: [
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/talha-khawaja-21000151/'
         }, {
-            name: "Experience",
-            data: "SunGard PS 2012 - 2017",
-        }, {
-            name: "Projects",
-            data: "Coming Soon!"
-        }, {
-            name: "Contact",
-            data: "Email: talhanaik@gmail.com"
+            name: 'GitHub',
+            url: 'https://github.com/talhakhawaja/'
         }
     ]
+
 }
