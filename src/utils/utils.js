1c59ea710f838ba3ce41795659edf5b06a13989f
TalhaKhawaja <talhanaik@gmail.com>
2021-05-15 18:59:26 -0400

    portfolio-v1: updated smooth scroll function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,7 +3,6 @@ import SmoothScroll from 'smooth-scroll';
 //Smooth scroll to the section when section link is clicked
 export function smoothScroll(name) {
     var scroll = new SmoothScroll();
-    var tagName = name.toLowerCase();
-    var anchor = document.getElementById(tagName);
+    var anchor = document.getElementById(name.toLowerCase());
     scroll.animateScroll(anchor);
 }
