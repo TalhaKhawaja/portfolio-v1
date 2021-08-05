041b43efcd2097b8b0b2c55069b87ca2f47ac7ca
TalhaKhawaja <talhanaik@gmail.com>
2021-08-05 17:06:14 -0400

    protfolio-v1: Updated the config and experience section to properly display the work experience with proper format

--- src/config.js
+++ src/config.js
@@ -14,44 +14,40 @@ module.exports = {
             url: "/#contact"
         }
     ],
-    sections: [
-        {
-            about: {
-                salutation: 'Hello!',
-                intro: 'I am Talha Khawaja',
-                info: 'I am a Software Engineer based in Long Island, NY',
-                additionalInfo: 'I have worked on frontend and backend development in my software engineering career'
-            },
-            experience: [{
-                startDate: 'Feburary 2017',
-                endDate: 'December 2020',
-                jobTitle: 'Software Engineer',
-                employer: 'PowerSchool Group',
-                city: 'Folsom, CA (Remote)',
-                description: [
-                    'Worked on PowerSchool Design System internal library to create and refine Polymer and Angular components',
-                    'Worked on Case Management using TypeScript and Polymer to create a functionality that allows users to track a student progress through the Special Education process.',
-                    'Developed an WPF AES key generator application and back-end encryption process to encrypt and decrypt specific values in the database using C#.',
-                    'Full stack software development using TypeScript, C#, MSSQL, HTML/CSS for our K-12 products.',
-                    'Worked on features and enhancements to Special Programs K-12 web application using C#, ASP.NET and JavaScript.',
-                    'Worked through all phases of the Agile software development life cycle including design, development, testing.'
-                ]
-            }, {
-                startDate: 'July 2013',
-                endDate: 'Feburary 2016',
-                jobTitle: 'Software Developer',
-                employer: 'SunGard K-12',
-                city: 'Ronkonkoma, NY',
-                description: [
-                    'Developed regulatory web forms in SunGard Special Education application using C#, ASP.NET, JavaScript and MSSQL.',
-                    'Worked on bug tracking, change requests, and task issues using Team Foundation Server.',
-                    'Provided unparalleled support customer support including bug resolution, data fixes and new installations and updates.',
-                    'Worked on data integration between SunGard SIS and Special Education K-12 products.',
-                    'Provided full stack software development using C#, SQL, JavaScript, jQuery, .NET and HTML/CSS'
-                ]
-            }]
-        }
-    ],
+    about: {
+        salutation: 'Hello!',
+        intro: 'I am Talha Khawaja',
+        info: 'I am a Software Engineer based in Long Island, NY',
+        additionalInfo: 'I have worked on frontend and backend development in my software engineering career'
+    },
+    experience: [{
+        startDate: 'Feburary 2017',
+        endDate: 'December 2020',
+        jobTitle: 'Software Engineer',
+        employer: 'PowerSchool Group',
+        city: 'Folsom, CA (Remote)',
+        description: [
+            'Worked on PowerSchool Design System internal library to create and refine Polymer and Angular components',
+            'Worked on Case Management using TypeScript and Polymer to create a functionality that allows users to track a student progress through the Special Education process.',
+            'Developed an WPF AES key generator application and back-end encryption process to encrypt and decrypt specific values in the database using C#.',
+            'Full stack software development using TypeScript, C#, MSSQL, HTML/CSS for our K-12 products.',
+            'Worked on features and enhancements to Special Programs K-12 web application using C#, ASP.NET and JavaScript.',
+            'Worked through all phases of the Agile software development life cycle including design, development, testing.'
+        ]
+    }, {
+        startDate: 'July 2013',
+        endDate: 'Feburary 2016',
+        jobTitle: 'Software Developer',
+        employer: 'SunGard K-12',
+        city: 'Ronkonkoma, NY',
+        description: [
+            'Developed regulatory web forms in SunGard Special Education application using C#, ASP.NET, JavaScript and MSSQL.',
+            'Worked on bug tracking, change requests, and task issues using Team Foundation Server.',
+            'Provided unparalleled support customer support including bug resolution, data fixes and new installations and updates.',
+            'Worked on data integration between SunGard SIS and Special Education K-12 products.',
+            'Provided full stack software development using C#, SQL, JavaScript, jQuery, .NET and HTML/CSS'
+        ]
+    }],
     socialMedia: [
         {
             name: 'LinkedIn',
--- src/sections/experience.section.jsx
+++ src/sections/experience.section.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import '../assets/styles/sections.scss';
-import { sections } from '../config'
+import VerticalTabs from '../components/tabpanel.component';
 
 const Experience = () => {
   return (
     <section id='experience'>
-      {sections &&
-        sections.map(({ experience }, index) => (
-          <div key={index}>
-            {experience &&
-              experience.map(({ startDate, endDate, jobTitle, employer, city, description }, i) => (
-                <div key={i}>
-                  <h2>{jobTitle} at {employer}, {city}</h2>
-                  <h3>{startDate} - {endDate}</h3>
-                  <ul>
-                    {description.map((desc, i) => (
-                      <li key={i}>{desc}</li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-          </div>
-        ))}
+        <VerticalTabs />
     </section>
   );
 }
